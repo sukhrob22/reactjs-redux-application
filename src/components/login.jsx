eff4dca895b33cd5a80451402197c231c86af06b
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-11-13 14:42:23 +0500

    Register & Login page & UI

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import { useState } from 'react';
+import { icon } from '../constants';
+import { Input } from '../ui';
 
 const Login = () => {
-    return <div>login</div>;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className='text-center mt-5'>
+            <main class='form-signin w-25 m-auto'>
+                <form>
+                    <img
+                        class='mb-2'
+                        src={icon}
+                        alt=''
+                        width='72'
+                        height='60'
+                    />
+                    <h1 class='h3 mb-3 fw-normal'>Please login</h1>
+                    <Input
+                        label={'Email address'}
+                        state={email}
+                        setState={setEmail}
+                        type={'email'}
+                    />
+                    <Input
+                        label={'Password'}
+                        state={password}
+                        setState={setPassword}
+                        type={'password'}
+                    />
+                    <button
+                        class='btn btn-primary w-100 py-2 mt-2'
+                        type='submit'
+                    >
+                        Login
+                    </button>
+                </form>
+            </main>
+        </div>
+    );
 };
 
 export default Login;
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import { useState } from 'react';
+import { icon } from '../constants';
+import { Input } from '../ui';
 
 const Register = () => {
-    return <div>Register</div>;
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    return (
+        <div className='text-center mt-5'>
+            <main class='form-signin w-25 m-auto'>
+                <form>
+                    <img
+                        class='mb-2'
+                        src={icon}
+                        alt=''
+                        width='72'
+                        height='60'
+                    />
+                    <h1 class='h3 mb-3 fw-normal'>Please register</h1>
+                    <Input label={'Username'} state={name} setState={setName} />
+                    <Input
+                        label={'Email address'}
+                        state={email}
+                        setState={setEmail}
+                        type={'email'}
+                    />
+                    <Input
+                        label={'Password'}
+                        state={password}
+                        setState={setPassword}
+                        type={'password'}
+                    />
+                    <button
+                        class='btn btn-primary w-100 py-2 mt-2'
+                        type='submit'
+                    >
+                        Register
+                    </button>
+                </form>
+            </main>
+        </div>
+    );
 };
 
 export default Register;
