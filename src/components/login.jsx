ec37d1daf16f6d8335a6f58b8554c485cb6f75da
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-11-16 11:57:24 +0500

    Navigation navbar

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { icon } from '../constants';
 import { Input } from '../ui';
 import { useDispatch, useSelector } from 'react-redux';
 import { signUserFailure, signUserStart, signUserSuccess } from '../slice/auth';
 import AuthService from '../service/auth';
 import { ValidationError } from './';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const dispatch = useDispatch();
-    const { isLoading } = useSelector((state) => state.auth);
+    const { isLoading, loggenIn } = useSelector((state) => state.auth);
+    const navigate = useNavigate();
 
     const loginHandler = async (e) => {
         e.preventDefault();
@@ -20,10 +22,17 @@ const Login = () => {
             const response = await AuthService.userLogin(user);
             // console.log(response);
             dispatch(signUserSuccess(response.user));
+            navigate('/');
         } catch (error) {
             dispatch(signUserFailure(error.response.data.errors));
         }
     };
+
+    useEffect(() => {
+        if (loggenIn) {
+            navigate('/');
+        }
+    }, []);
 
     return (
         <div className='text-center mt-5'>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,41 @@
 import { Link } from 'react-router-dom';
 import { logo } from '../constants';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+    const { loggenIn, user } = useSelector((state) => state.auth);
     return (
         <div className='d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom container pt-3'>
             <Link to={'/'}>
                 <img src={logo} alt='' />
             </Link>
             <nav className='d-inline-flex mt-2 mt-md-0 ms-md-auto'>
-                <Link
-                    to={'/login'}
-                    className='me-3 py-2 link-body-emphasis text-decoration-none'
-                >
-                    Login
-                </Link>
-                <Link
-                    to={'/register'}
-                    className='me-3 py-2 link-body-emphasis text-decoration-none'
-                >
-                    Register
-                </Link>
+                {loggenIn ? (
+                    <>
+                        <p className='me-3 py-2 m-0 link-body-emphasis text-decoration-none'>
+                            {' '}
+                            {user.username}{' '}
+                        </p>
+                        <button className='btn btn-outline-danger'>
+                            Logout
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <Link
+                            to={'/login'}
+                            className='me-3 py-2 link-body-emphasis text-decoration-none'
+                        >
+                            Login
+                        </Link>
+                        <Link
+                            to={'/register'}
+                            className='me-3 py-2 link-body-emphasis text-decoration-none'
+                        >
+                            Register
+                        </Link>
+                    </>
+                )}
             </nav>
         </div>
     );
